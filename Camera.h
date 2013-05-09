095080b4fb4ab6f576e8d1996eba3a01ca85f05a
Turgure <toranana_bye@yahoo.co.jp>
2013-05-09 23:46:48 +0900

    微調整

--- Camera.h
+++ Camera.h
@@ -1,7 +1,5 @@
 ﻿#pragma once
-#include<DxLib.h>
-
-
+#include <DxLib.h>
 
 class Camera{
 public:
@@ -12,30 +10,27 @@ public:
 	void setX(float x){ pos.x = x; }
 	void setY(float y){ pos.y = y; }
 	void setZ(float z){ pos.z = z; }
-	
-	//カメラの注視点をセットする
-	void setTargetX(float x){ target.x = x; }
-	void setTargetY(float y){ target.y = y; }
-	void setTargetZ(float z){ target.z = z; }
-
-	//カメラのポジションを返す
 	float getX(){ return pos.x; }
 	float getY(){ return pos.y; }
 	float getZ(){ return pos.z; }
 
-		//カメラの注視点を返す
+	//カメラの注視点をセットする
+	void setTargetX(float x){ target.x = x; }
+	void setTargetY(float y){ target.y = y; }
+	void setTargetZ(float z){ target.z = z; }
 	float getTargetX(){ return target.x; }
 	float getTargetY(){ return target.y; }
 	float getTargetZ(){ return target.z; }
 
-
 private:
 	enum CameraPos{
-	ZERO_ZERO,
-	ZERO_MAX,
-	MAX_ZERO,
-	MAX_MAX
+		ZERO_ZERO,
+		ZERO_MAX,
+		MAX_ZERO,
+		MAX_MAX
 	} viewfrom;	//カメラの位置(右下,左下,右上,左上)
 	VECTOR pos;		// カメラの座標
 	VECTOR target;	// カメラの注視点
+
+	static const int difffromcameratotarget = 100;
 };
