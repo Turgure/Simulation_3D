8f8fe2b22f6c514b1cb81b36bb5d3e63dd6a140f
Turgure <toranana_bye@yahoo.co.jp>
2013-05-24 01:00:51 +0900

    窓のタイトル追加

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	// ウインドウモードに変更
 	ChangeWindowMode(true);
 	if(DxLib_Init() == -1 || SetDrawScreen(DX_SCREEN_BACK) != 0) return -1;	//初期化と裏画面処理
+	SetMainWindowText("tannpo");
 
 	Keyboard::initialize();
 	Sound::initialize();
