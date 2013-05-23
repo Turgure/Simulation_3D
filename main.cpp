5bb12a6f94c8437d31c714552856595060a4edd0
TANNPOSP <yuutae07@gmail.com>
2013-05-24 01:33:29 +0900

    タイトル

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 #include "DxLib.h"
 #include "Sound.h"
 #include "Keyboard.h"
-#include "BattleScene.h"
 #include "StartScene.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
 	// ウインドウモードに変更
 	ChangeWindowMode(true);
 	if(DxLib_Init() == -1 || SetDrawScreen(DX_SCREEN_BACK) != 0) return -1;	//初期化と裏画面処理
-	SetMainWindowText("tannpo");
+	SetMainWindowText("neo タクティクス");
 
 	Keyboard::initialize();
 	Sound::initialize();
